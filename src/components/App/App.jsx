2d63210dcc8c2b8ce3821b08b0758e463f38cc0f
veronika24di <veronikanof909@gmail.com>
2023-05-11 11:11:32 +0300

    hook

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,76 +1,72 @@
-import { Component } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import SearchBar from 'components/Searchbar/Searchbar';
-import ImageGallery from 'components/ImageGallery/ImageGallery';
+import { ImageGallery } from 'components/ImageGallery/ImageGallery';
 import ButtonLoadMore from 'components/Button/Button';
 import Loader from 'components/Loader/Loader';
-import Modal from 'components/Modal/Modal';
+import { Modal } from 'components/Modal/Modal';
 import styles from './App.module.css';
 import imagesFetch from 'Api/FetchImages';
 
-export default class App extends Component {
-  state = {
-    images: [],
-    currentPage: 1,
-    searchQuery: '',
-    isLoading: false,
-    error: null,
-    showModal: false,
-    modalUrl: '',
+export const App = () => {
+  const [images, setImages] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [modalUrl, setModalUrl] = useState('');
+
+  const onChangeSerchQuery = query => {
+    setImages([]);
+    setCurrentPage(1);
+    setSearchQuery(query);
+    setError(null);
   };
 
-  componentDidUpdate(_, prevState) {
-    if (this.state.searchQuery !== prevState.searchQuery) {
-      this.fetchImages();
-    }
-  }
-
-  onChangeSerchQuery = query => {
-    this.setState({
-      images: [],
-      currentPage: 1,
-      searchQuery: query,
-      error: null,
-    });
-  };
-
-  fetchImages = async () => {
-    this.setState({ isLoading: true });
+  const fetchImages = useCallback(async () => {
+    setIsLoading(true);
     try {
-      const { currentPage, searchQuery } = this.state;
       const response = await imagesFetch({
         page: currentPage,
-        searchQuery: searchQuery,
+        searchQuery,
       });
-
-      this.setState(prevState => ({
-        images: [...prevState.images, ...response],
-        currentPage: prevState.currentPage + 1,
-      }));
+      setImages(prev => [...prev, ...response]);
     } catch (error) {
-      this.setState({ error });
+      setError(error);
     } finally {
-      this.setState({ isLoading: false });
+      setIsLoading(false);
     }
+  }, [currentPage, searchQuery]);
+
+  const toggleModal = largeUrl => {
+    setShowModal(!showModal);
+    setModalUrl(largeUrl);
+  };
+  const handleClickLoadMore = () => {
+    setCurrentPage(prev => prev + 1);
   };
 
-  toggleModal = largeUrl => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-      modalUrl: largeUrl,
-    }));
-  };
-  render() {
-    const { images, isLoading, showModal, modalUrl } = this.state;
-    return (
-      <div className={styles.app}>
-        <SearchBar onSubmit={this.onChangeSerchQuery} />
-        <ImageGallery images={images} onClick={this.toggleModal} />
-        {images.length % 12 < 1 && images.length > 0 && (
-          <ButtonLoadMore onClick={this.fetchImages} />
-        )}
-        <Loader loading={isLoading} />
-        {showModal && <Modal url={modalUrl} toggleModal={this.toggleModal} />}
-      </div>
-    );
-  }
-}
+  useEffect(() => {
+    if (searchQuery) {
+      fetchImages();
+    }
+  }, [searchQuery, currentPage, fetchImages]);
+
+  useEffect(() => {
+    if (error) {
+      console.log('Warning');
+    }
+  }, [error]);
+
+  return (
+    <div className={styles.app}>
+      <SearchBar onSubmit={onChangeSerchQuery} />
+      <ImageGallery images={images} onClick={toggleModal} />
+      {images.length % 12 < 1 && images.length > 0 && (
+        <ButtonLoadMore onClick={handleClickLoadMore} />
+      )}
+      <Loader loading={isLoading} />
+      {showModal && <Modal url={modalUrl} toggleModal={toggleModal} />}
+    </div>
+  );
+};
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,39 +1,37 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import React, { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import styles from './Modal.module.css';
 const modalRoot = document.querySelector('#modal');
 
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeydown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeydown);
-  }
-  handleKeydown = event => {
-    if (event.code === 'Escape') {
-      this.props.toggleModal();
-    }
-  };
-
-  handleBackdropClick = event => {
+export const Modal = ({ toggleModal, url }) => {
+  const handleBackdropClick = event => {
     if (event.target === event.currentTarget) {
-      this.props.toggleModal();
+      toggleModal();
     }
   };
-  render() {
-    return createPortal(
-      <div className={styles.overlay} onClick={this.handleBackdropClick}>
-        <div className={styles.modal}>
-          <img src={this.props.url} alt="" />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
-}
+
+  useEffect(() => {
+    const handleKeydown = e => {
+      if (e.code === 'Escape') {
+        toggleModal();
+      }
+    };
+    window.addEventListener('keydown', handleKeydown);
+    return () => {
+      window.removeEventListener('keydown', handleKeydown);
+    };
+  }, [toggleModal]);
+
+  return createPortal(
+    <div className={styles.overlay} onClick={handleBackdropClick}>
+      <div className={styles.modal}>
+        <img src={url} alt="" />
+      </div>
+    </div>,
+    modalRoot
+  );
+};
 Modal.propTypes = {
   toggleModal: PropTypes.func,
   url: PropTypes.string,
